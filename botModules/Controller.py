4d30e12a35f576164928793358b309decd48c37d
superRaptor911 <raptor.inc2018@gmail.com>
2021-05-20 21:51:50 +0530

    Multiple Image and video support

--- botModules/Controller.py
+++ botModules/Controller.py
@@ -1,8 +1,8 @@
 from botModules import ThreadSaver
-# from botModules import Debug
+from botModules import Debug
 
 def evalCommand(api, originTweet, command):
     if command["command"] in ["save", "sav"]:
         ThreadSaver.main(api, originTweet, command["args"])
-    # elif command["command"] in ["debugInfo", "debug"]:
-    #     Debug.main(api, originTweet, command["args"])
+    elif command["command"] in ["debugvideo", "adhkh"]:
+        Debug.main(api, originTweet, command["args"])
